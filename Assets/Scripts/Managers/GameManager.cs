96745e4fa2cd4a0f704528af8b8c6127f5dbc16a
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2023-04-19 18:43:07 -0700

    Added animation controller as game state machine

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 
 using Unity.Netcode;
+using UnityEngine;
 
 using InterruptingCards.Models.Abstract;
 using InterruptingCards.Models.PlayingCards;
@@ -9,16 +10,27 @@ namespace InterruptingCards.Managers
 {
     public class GameManager : NetworkBehaviour, IGame
     {
+        private const int GameStateLayer = 0;
+
+        private readonly int _playingStateId = Animator.StringToHash("Game.Playing");
+        private readonly int _playingParamId = Animator.StringToHash("playing");
+
+        private readonly NetworkManagerDecorator _networkManager = NetworkManagerDecorator.Singleton;
         private readonly List<Player> _players = new();
+
+        [SerializeField] private Animator _gameStateMachine;
+
         private Deck<Suit, Rank> _deck;
+
+        private bool IsPlaying { get { return _gameStateMachine.GetCurrentAnimatorStateInfo(GameStateLayer).fullPathHash == _playingStateId; } }
 
         public override void OnNetworkSpawn()
         {
-            NetworkManagerDecorator.Singleton.OnClientConnectedCallback -= AddPlayer;
-            NetworkManagerDecorator.Singleton.OnClientConnectedCallback += AddPlayer;
+            _networkManager.OnClientConnectedCallback -= AddPlayer;
+            _networkManager.OnClientConnectedCallback += AddPlayer;
 
-            NetworkManagerDecorator.Singleton.OnClientDisconnectCallback -= RemovePlayer;
-            NetworkManagerDecorator.Singleton.OnClientDisconnectCallback += RemovePlayer;
+            _networkManager.OnClientDisconnectCallback -= RemovePlayer;
+            _networkManager.OnClientDisconnectCallback += RemovePlayer;
         }
         
         public void EndTurn()
@@ -28,16 +40,33 @@ namespace InterruptingCards.Managers
 
         public void Play()
         {
-            throw new System.NotImplementedException();
+            _gameStateMachine.SetBool(_playingParamId, true);
         }
 
         private void AddPlayer(ulong clientId)
         {
+            if (IsPlaying)
+            {
+                Debug.LogWarning($"Attempted to add player ({clientId}) while game is playing");
+                return;
+            }
             _players.Add(new Player(clientId.ToString(), new Hand()));
         }
 
         private void RemovePlayer(ulong clientId)
         {
+            if (!IsServer)
+            {
+                Debug.LogWarning($"Client attempted to remove player ({clientId})");
+                return;
+            }
+
+            if (IsPlaying)
+            {
+                Debug.LogWarning($"Player ({clientId}) removed while game is playing");
+                return;
+            }
+
             _players.RemoveAll(p => p.Name == clientId.ToString());
         }
     }
